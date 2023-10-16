b6590708275888db8a85aaedb9c6dd58e6a78199
davidcoats <david@getsafetycone.com>
2023-10-16 14:37:34 -0700

    Initial commit.

---
+++ source/R5T.S0108/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0108
+{
+	/// <summary>
+	/// Dotnet runtime scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.S0108/Code/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace R5T.S0108
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
